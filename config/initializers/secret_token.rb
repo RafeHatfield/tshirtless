9ae3fb7577b739b6c7f143817c7803dcbe23b32e
RafeHatfield <rafe.hatfield@suite101.com>
2013-02-23 12:44:50 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tshirtless::Application.config.secret_token = 'e60f817eff3fe865e01279bb86e695ae2a052090f6d833b34db632b31f440f25586ffd23914b8aee49393737d45219bc98e627fb1a07a3529b534bb5d52ce11b'
